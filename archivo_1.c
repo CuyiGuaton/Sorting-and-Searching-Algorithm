b3016c94a89845d5efae7750dbfca960eeb0a5c9
Sergio Salinas <sergiosalfer@gmail.com>
2017-04-20 14:31:25 -0300

    crea secuencais

--- archivo_1.c
+++ archivo_1.c
@@ -17,7 +17,7 @@ int main(int argc, char const *argv[]) {
   if (inputfile) {
       for ( int i=0; (c = getc(inputfile)) != EOF; i++){
           a[i] = c;
-          //putchar(a[i]);
+          putchar(a[i]);
           largo++;
       }
       fclose(inputfile);
@@ -28,12 +28,6 @@ int main(int argc, char const *argv[]) {
   int key[4] = {0,0,0,0};
   int aux = 0;
   for (size_t i = 0; i < 1000; i++) {
-    /*
-      key[0]= i/1000 >=1 ? i/1000 : 0 ;
-      key[1]= i/100 >=1 ? i/100 : 0 ;
-      key[2]= i/10 >=1 ? i/10 : 0 ;
-      key[3]= i % 10;
-      */
       aux = i;
       key[3]= aux/10 <100 ? aux%10 : 0 ;
       aux/=10;
@@ -41,11 +35,12 @@ int main(int argc, char const *argv[]) {
       aux/=10;
       key[1]= aux/10 <1 ? aux%10 : 0 ;
       key[0]= aux%10;
-
+      /*
       for (size_t j = 0; j < 4; j++) {
         printf("%i", key[j]);
       }
       printf("\n");
+      */
   }
 
 

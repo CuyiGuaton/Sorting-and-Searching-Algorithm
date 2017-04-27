4bb86692d1f0f6315baeb277ac4fc98e9ec16aa6
Sergio Salinas <sergiosalfer@gmail.com>
2017-04-27 17:23:14 -0300

    separa en 6 el string muestra de los 0 de adelante

--- archivo_3.c
+++ archivo_3.c
@@ -31,7 +31,7 @@ int main(int argc, char const *argv[]) {
   fragments = malloc(sizeof(*fragments) * largo / 6);
   for (size_t i = 0; i < largo/6; i++) {
     fragments[i] = atoi(strndup(a + 6 * i, 6));
-    printf("%i\n", fragments[i]);
+    printf("%06i\n", fragments[i]);
   }
  /*
   char key[2]; //key es de largo 5 para dejar espacio al end of string character

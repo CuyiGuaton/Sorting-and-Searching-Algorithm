5649d0015d151677005b75818fdc29d0992d0463
Sergio Salinas <sergio.salinas@usach.cl>
2017-05-04 01:26:43 -0300

    pico

--- archivo_2.c
+++ archivo_2.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <time.h>
 
+
 int main(int argc, char const *argv[]) {
   FILE *inputfile;
   int c;
@@ -17,7 +18,7 @@ int main(int argc, char const *argv[]) {
   if (inputfile) {
       for ( int i=0; (c = getc(inputfile)) != EOF; i++){
           a[i] = c - '\0';
-          putchar(a[i]);
+        //  putchar(a[i]);
           largo++;
       }
       fclose(inputfile);
@@ -26,21 +27,18 @@ int main(int argc, char const *argv[]) {
   printf("El largo de la mierda es %i\n", largo);
 
   /* Desde aquí empieza el proceso de busqueda del número de 4 digitos que más se repite */
-    int *hexarray = (int*)malloc(1000*sizeof(int)); // crea un arreglo que va del 0 al 9999
-    int freq = 1;
+    int *hexarray = (int*)malloc(10000*sizeof(int)); // crea un arreglo que va del 0 al 9999
     char key[2]; // key guarda temporalmente un strig de 4 caracteres del arreglo
-    int number; // convierte el string obtenido en número
-    char * pEnd;
     int count = 0;
     for (int i = 0; i < largo-4; i++) {
       strncpy(key, a, 4); // se copia un string de largo 4 del largo a a key
-      hexarray[i] = strtol(key, &pEnd, 16); // se tranforma el string obtenido a int hexadecimal
-      printf("%i = %04X\n", i,hexarray[i] );
+      hexarray[i] = strtol(key, NULL, 16); // se tranforma el string obtenido a int hexadecimal
+      printf("hexarray[%i] = %04X\n", i,hexarray[i] );
       count++;
       a+=1; // se avanza en 1 el punto del arreglo a, así se va a avanzando de posición en 1 en a.
     }
 
-  free(a);
-  free(hexarray);
+
+    free(hexarray);
   return 0;
 }

fee73a30cc9f00ea21a956e03fb5f2c268e7ef9f
Danilo <daniloabella@gmail.com>
2017-05-04 03:07:43 -0300

    Prog. 2 + Salida en 2.txt

--- archivo_2.c
+++ archivo_2.c
@@ -24,19 +24,79 @@ int main(int argc, char const *argv[]) {
       fclose(inputfile);
   }
 
-  printf("El largo de la mierda es %i\n", largo);
+  printf("El largo es: %i\n\n", largo);
+
 
   /* Desde aquí empieza el proceso de busqueda del número de 4 digitos que más se repite */
     int *hexarray = (int*)malloc(10000*sizeof(int)); // crea un arreglo que va del 0 al 9999
     char key[2]; // key guarda temporalmente un strig de 4 caracteres del arreglo
-    int count = 0;
-    for (int i = 0; i < largo-4; i++) {
+    int count = 0,i;
+    for (i = 0; i < largo-4; i++) {
       strncpy(key, a, 4); // se copia un string de largo 4 del largo a a key
       hexarray[i] = strtol(key, NULL, 16); // se tranforma el string obtenido a int hexadecimal
-      printf("hexarray[%i] = %04X\n", i,hexarray[i] );
+//      printf("hexarray[%i] = %04X\n", i,hexarray[i] );
       count++;
       a+=1; // se avanza en 1 el punto del arreglo a, así se va a avanzando de posición en 1 en a.
     }
+
+	// Ordenamiento por Insercion
+
+	int temp, j;
+
+	for (i = 1; i < largo-4; i++)
+	{
+		temp = hexarray[i];
+		j = i - 1;
+		while ( (hexarray[j] > temp) && (j >= 0) )
+		{
+			hexarray[j + 1] = hexarray[j];
+			j--;
+		}
+		hexarray[j + 1] = temp;
+	}
+
+	
+	// Mostrar Orden
+	
+	 printf("\n\nOrdenado por Inserción\n\n");
+
+	for (i = 0; i < largo-4; i++) 
+	{
+		printf("hexarray[%i] = %04X\n", i,hexarray[i] );
+	}
+
+	// Mostrar cantidad de repeticiones
+
+	printf("\n\nCantidad de Repeticiones\n\n");
+
+	int cont=1;
+
+	temp=0;
+	for (i = 0; i < largo-4; i++) 
+	{
+
+		if(hexarray[i]!=temp && cont>1 )
+		{	
+			printf("\nLa secuencia: %04X\n se repite %i veces.", hexarray[i],cont );
+			cont=1;
+		
+		}
+
+		if ( temp==hexarray[i] && i!=0 )
+		{ 
+			cont++; 
+		}
+		else
+		{
+			cont=1;
+		}	
+
+		temp=hexarray[i];
+
+	}
+
+
+
 
 
     free(hexarray);
